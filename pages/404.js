bf4f698b8b8de64bdc4cd034663b6b2d8310b845
Riya Gupta git config --global user.email riyag0841@gmail.com <riyag0841@gmail.com>
2021-08-05 15:46:46 +0530

    Added 404 page in the website

---
+++ pages/404.js
@@ -0,0 +1,18 @@
+import Layout from "./components/Layout";
+import Link from "next/link"
+import styles from "../styles/404.module.css"
+
+function NotFound() {
+    return (
+        <Layout>
+        <section className={styles.notFoundSection}>
+        <img src="https://images.unsplash.com/photo-1623018035782-b269248df916?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80" className={styles.image} alt="notfound" />
+        <h1>Not found!</h1>
+        <p>Oops. Seems like this page does not exist :( </p>
+       <span> Go back to the <Link href="/"><a className={styles.redirecting}>home page</a></Link> </span>
+       </section>
+        </Layout>
+    )
+}
+
+export default NotFound
